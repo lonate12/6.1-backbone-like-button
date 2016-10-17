1dca18c8b1fc7a779913a768d43a1cd18727026a
Luis Rene Onate <lonate12@gmail.com>
2016-10-17 14:39:13 -0400

    Completed exercise.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+var models = require('./models/like.js');
+var $ = require('jquery');
+
+var likeButton = new models.LikeButton();
+
+$('#like-btn').on('click', function(event){
+  event.preventDefault();
+  likeButton.like();
+  likeButton.toJSON();
+});
--- app/scripts/models/like.js
+++ app/scripts/models/like.js
@@ -1,13 +1,25 @@
 var Backbone = require('backbone');
+var $ = require('jquery');
 
 var LikeButton = Backbone.Model.extend({
   defaults: {
     likes: 0,
-    like: function(){
-      this.set('likes', (this.get('likes') + 1));
-    }
   },
   initialize: function(){
-    $('#like-btn').textContent(this.get('likes'));
+    $('#num-span').text(this.get('likes'));
+  },
+  like: function(){
+    this.set('likes', (this.get('likes') + 1));
+    $('#num-span').text(this.get('likes'));
+    $('#like-span').text(' Likes');
+  },
+  toJSON: function(){
+    if(this.get('likes') === 1){
+      $('#like-span').text('Like');
+    }
   }
 });
+
+module.exports = {
+  'LikeButton': LikeButton
+};
